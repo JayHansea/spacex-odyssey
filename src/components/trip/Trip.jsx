62c943d080c9247e3808dc927436782fdf4ae542
Jay Hansea <ezeokafortochukwu@gmail.com>
2023-11-19 23:54:47 +0100

    managed state with redux toolkit

--- src/components/trip/Trip.jsx
+++ src/components/trip/Trip.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import { View, Text, Pressable } from "react-native";
 import { SelectList } from "react-native-dropdown-select-list";
+import { useDispatch, useSelector } from "react-redux";
 import styles from "./Trip.Style";
 import { SIZES } from "../../constants/theme";
+import { tripActions } from "../../store/tripSlice";
 
 const Trip = () => {
-  const [departureStation, setDepartureStation] = useState("");
-  const [destination, setDestination] = useState("");
-  const [spacecraft, setSpacecraft] = useState("");
-  const [fare, setFare] = useState(0);
+  const dispatch = useDispatch();
+  const { departureStation, destination, spacecraft, fare } = useSelector(
+    (state) => state.trip
+  );
 
   const categories = [
     { key: "abuja", value: "Abuja" },
@@ -68,25 +70,32 @@ const Trip = () => {
       const isCrossOrbit = orbit[departureStation] !== orbit[destination];
 
       if (isCrossOrbit) {
-        setFare((fare += 250)); // Anytime you cross from one orbit to another on Falcon 1 = 250btc
+        fare += 250; // Anytime you cross from one orbit to another on Falcon 1 = 250btc
       } else {
-        setFare((fare += 50)); // Journey between two points in an orbit on Falcon 1 = 50btc
+        fare += 50; // Journey between two points in an orbit on Falcon 1 = 50btc
       }
 
       if (spacecraft === "F9") {
-        setFare((fare *= 2)); // Falcon 9 is a luxury spacecraft with better experience. Its passengers pay twice the fare of the Falcon 1.
+        fare *= 2; // Falcon 9 is a luxury spacecraft with better experience. Its passengers pay twice the fare of the Falcon 1.
       }
     }
 
     if (type[destination] === "manmade") {
-      setFare((fare += 200)); // Royalty when landing on man-made stations (satellites) on any rocket = 200btc
+      fare += 200; // Royalty when landing on man-made stations (satellites) on any rocket = 200btc
     }
 
     if (sameStation) {
-      setFare((fare = 0));
+      fare = 0;
     }
 
-    setFare(fare);
+    dispatch(tripActions.setFare(fare));
+  };
+
+  const bookTrip = () => {
+    // Perform any necessary actions to book the trip
+    // You can dispatch additional actions or make API calls here
+    // For now, just reset the trip
+    dispatch(tripActions.resetTrip());
   };
 
   return (
@@ -100,30 +109,37 @@ const Trip = () => {
       </View>
       <View style={styles.selectView}>
         <SelectList
-          setSelected={(val) => setDepartureStation(val)}
+          setSelected={(val) => dispatch(tripActions.setDepartureStation(val))}
           data={categories}
           placeholder="Select Departure Station"
           onSelect={tripFare}
         />
         <SelectList
-          setSelected={(val) => setDestination(val)}
+          setSelected={(val) => dispatch(tripActions.setDestination(val))}
           data={subcategories[departureStation]}
           placeholder="Choose Destination"
           boxStyles={{ marginTop: SIZES.medium }}
           onSelect={tripFare}
         />
         <SelectList
-          setSelected={(val) => setSpacecraft(val)}
+          setSelected={(val) => dispatch(tripActions.setSpacecraft(val))}
           data={spacecrafts}
           placeholder="Select Desired Spacecraft"
           boxStyles={{ marginTop: SIZES.medium }}
           onSelect={tripFare}
         />
       </View>
+      <View style={styles.noteView}>
+        <Text style={styles.note}>
+          Choose departure, destination and spacecraft to see fare
+        </Text>
+      </View>
       <View style={styles.fareView}>
         <Text style={styles.fare}>Fare: {fare} btc</Text>
         <Pressable style={styles.button}>
-          <Text style={styles.buttonText}>Book Trip</Text>
+          <Text style={styles.buttonText} onPress={bookTrip}>
+            Book Trip
+          </Text>
         </Pressable>
       </View>
     </View>
--- src/store/tripSlice.js
+++ src/store/tripSlice.js
@@ -1,9 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  departureStation: "",
+  destination: "",
+  spacecraft: "",
+  fare: 0,
+};
+
 const tripSlice = createSlice({
   name: "trip",
-  initialState: {},
-  reducers: {},
+  initialState,
+  reducers: {
+    setDepartureStation: (state, action) => {
+      state.departureStation = action.payload;
+    },
+    setDestination: (state, action) => {
+      state.destination = action.payload;
+    },
+    setSpacecraft: (state, action) => {
+      state.spacecraft = action.payload;
+    },
+    setFare: (state, action) => {
+      state.fare = action.payload;
+    },
+    resetTrip: (state) => {
+      state.departureStation = "";
+      state.destination = "";
+      state.spacecraft = "";
+      state.fare = 0;
+    },
+  },
 });
 
 export const tripActions = tripSlice.actions;
